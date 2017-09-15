b19435fada53b3a0826860bdbe529053b7f8fd6b
sebastian-meier <contact@sebastianmeier.eu>
2017-09-15 13:30:36 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # tsb-tileserver
-OpenSource TileServer implementation
+
+This is a simple self-hosted tile server implementation to work with the mapbox-gl-js frontend library. While this repo contains the structure and code to run the server, it does not contain the actual tiles you need to deliver data to the frontend. See below.
+
+## Setup
+
+```
+npm install
+```
+
+Add your tiles, glyphs and sprites and then add them to the config.json.
+
+## Running
+
+There are two modes, one which allows all connections:
+
+```
+node index.js true
+```
+
+and one which runs a CORS check on provided domains (config.json):
+
+```
+node index.js
+```
+
+## Building Tiles and stuff
+
+### Tiles
+While you can of course build your own tiles, the easiest way is going to https://openmaptiles.com/ and get your vector tile extract. 
+You can then simply add this to your tiles folder.
+
+### Glyphs
+Depending on the style you use, you will need glyphs. You can use the genfontgl module to do so, see here for an example: https://github.com/openmaptiles/fonts
+
+### Sprites
+The same accounts for sprites, if you have a folder of SVGs, use spritezero to convert them into a sprite.png/json.
+
+## Deploy
+Once the server is running and you have added glyphs and sprites to the according folders you are ready to go. Check the 'client' folder for an example of how to use the vector tiles with mapbox-gl-js.
---
+++ index.js
@@ -0,0 +1,107 @@
+require('sqlite3').verbose()
+
+var config = require('./config.json'),
+  express = require('express'),
+  http = require('http'),
+  app = express(),
+  tilelive = require('tilelive')
+
+require('mbtiles').registerProtocols(tilelive)
+
+var sources = config.sources
+
+var source_ref = {};
+for(var i in sources){
+  source_ref[sources[i].name] = parseInt(i)
+}
+
+var source_count = 0
+
+function loadTiles(){
+  tilelive.load('mbtiles://'+ __dirname + sources[source_count].url, function(err, source) {
+    if (err) { throw err;}
+
+    sources[source_count].source = source
+
+    if(source_count < (sources.length-1)){
+      source_count++
+      loadTiles()
+    }else{
+      startServer()
+    }
+  })
+}
+
+loadTiles()
+
+function startServer(){
+  app.set('port', 10060);
+
+  app.use((req, res, next) => {
+    var origins = config.origins
+    for(var i=0;i<origins.length;i++){
+      var origin = origins[i]
+      if("origin" in req.headers){
+        if(req.headers.origin.indexOf(origin) > -1){
+          res.header("Access-Control-Allow-Origin", "*")
+        }
+      }
+      if("referer" in req.headers){
+        if(req.headers.referer.indexOf(origin) > -1){
+          res.header("Access-Control-Allow-Origin", "*")
+        }
+      }
+    }
+
+    if(process.argv.length >= 3 && process.argv[2] === "true"){
+      res.header("Access-Control-Allow-Origin", "*")
+    }
+
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+  })
+
+  app.use('/glyphs', express.static('glyphs'))
+  app.use('/sprites', express.static('sprites'))
+
+  app.get(/^\/tiles\/(.*)\/(\d+)\/(\d+)\/(\d+).pbf$/, (req, res) => {
+    deliverTile(req, res, 'vector')
+  })
+
+  app.get(/^\/tiles\/(.*)\/(\d+)\/(\d+)\/(\d+).png$/, (req, res) => {
+    deliverTile(req, res, 'raster')
+  });
+
+  http.createServer(app).listen(app.get('port'), function() {
+      console.log('Express server listening on port ' + app.get('port'));
+  });
+}
+
+function deliverTile(req, res, type){
+  var type = req.params[0]
+      
+  var good = false
+  if(type in source_ref){
+    if(sources[source_ref[type]].type === type){
+      good = true
+    }
+  }
+  
+  if(!good){
+    //TODO: Add an empty type tileset!
+  }
+
+  var z = req.params[1],
+    x = req.params[2],
+    y = req.params[3];
+
+  sources[source_ref[type]].source.getTile(z, x, y, (err, tile, headers) => {
+    if (err) {
+      res.set(headers)
+      res.send('')
+    } else {
+      res.set(headers)
+      res.send(tile)
+    }
+  })
+}
